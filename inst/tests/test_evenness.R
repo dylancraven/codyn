b4f98b67832152c1685c11d0868875ff57a2c772
Meghan Avolio <meghanavolio@Meghans-MB.local>
2017-11-19 19:43:37 -0500

    test of evenness

--- inst/tests/test_evenness.R
+++ inst/tests/test_evenness.R
@@ -8,9 +8,24 @@ test_that("evenness loads and returns correct result", {
     # Test if evenness produces correct values with count data
     # Some abundance values are 0, and some are missing
     x <- c(2,3,4,5,8,9,11,0,0,23,11,2,1,4)
-    result <- e_q(x)
+    result <- evenness(x)
     expect_that(length(result), equals(1))
     expect_that(class(result), matches("numeric"))
     expect_that(result, equals(0.2033371, tolerance=0.0001))
+    
+    # Test if evenness produces NA with only 1 species
+    x <- c(99)
+    result <- evenness(x)
+    expect_that(result, equals(NA))
+    
+    # Test if evenness produces 1 in a perfectly even community
+    x <- c(15,15,15)
+    result <- evenness(x)
+    expect_that(length(result), equals(1))
+    expect_that(class(result), matches("numeric"))
+    expect_that(result, equals(1))
+    
+    # Test if richness produces a error with non-numeric data
+    expect_error(evenness(unique(iris$Species)))
     
 })
