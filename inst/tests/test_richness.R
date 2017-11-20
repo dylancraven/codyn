146566f271225856667d8a1d8a54e61ac004792f
Meghan Avolio <meghanavolio@Meghans-MB.local>
2017-11-19 19:43:44 -0500

    test of richness

--- inst/tests/test_richness.R
+++ inst/tests/test_richness.R
@@ -13,16 +13,7 @@ test_that("richness loads and returns correct result", {
     expect_that(class(result), matches("integer"))
     expect_that(result, equals(12))
     
-    # Test if richness produces correct values with factor data
-    result <- richness(unique(iris$Species))
-    expect_that(length(result), equals(1))
-    expect_that(class(result), matches("integer"))
-    expect_that(result, equals(3))
-    
-    # Test if richness produces correct values with species lists data
-    result <- richness(unique(knz_001d$species))
-    expect_that(length(result), equals(1))
-    expect_that(class(result), matches("integer"))
-    expect_that(result, equals(84))
-    
+    # Test if richness produces a error with non-numeric data
+    expect_error(richness(unique(iris$Species)))
+
 })
