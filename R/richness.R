403680a71797c6ab6680b5deeae59bb61a04e17b
Meghan Avolio <meghanavolio@Meghans-MB.local>
2017-11-19 19:43:14 -0500

    richness function description

--- R/richness.R
+++ R/richness.R
@@ -1,24 +1,17 @@
 #' @title Richness
-#' @description A measure of the relative change in species rank abundances, which indicates shifts in relative abundances over time (Collins et al. 2008).
-#' Mean rank shifts are calculated as the average difference in species' ranks between consecutive time periods, among species that are present across the entire time series.
-#' @param x the vector of abundances of each species
-#' @return rank_shift returns a data frame with the following columns:
-#' \itemize{
-#'  \item{time.var_pair: }{A factor column that returns the two time periods being compared, separated by a dash. The name of this column is the same as the time.var column in the input dataframe followed by "_pair".}
-#'  \item{MRS: }{A numeric column with the mean rank shift values.}
-#'  \item{replicate.var: }{A column that has same name and type as the replicate.var column, if replication is specified.}
-#' }
+#' @description A count of the number of species in a sample.
+#' @param x the vector of abundances of each species in a sample
+#' @return an integer
 #' @details
-#' The input data frame needs to contain columns for time, species and abundance; time.var, species.var and abundance.var are used to indicate which columns contain those variables.
-#' If multiple replicates are included in the data frame, that column should be specified with replicate.var. Each replicate should reflect a single experimental unit - there must be a single abundance value per species within each time point and replicate.
-#' @references  Collins, Scott L., Katharine N. Suding, Elsa E. Cleland, Michael Batty, Steven C. Pennings, Katherine L. Gross, James B. Grace, Laura Gough, Joe E. Fargione, and Christopher M. Clark.  (2008) "Rank clocks and plant community dynamics." Ecology 89, no. 12: 3534-41.
+#' For numeric data only, richness will count the number of species in vector.
+#' @references
 #' @examples
 #'  richness(c(2,3,4,5,8,9,11,0,0,23,11,2,1,NA,NA,4)) # Numeric count data
-#'  richness(unique(iris$Species))  # Factor data
-#'  richness(as.character(unique(iris$Species)))  # Species list
+#'  richness(subset(knz_001d, year==1983&subplot=="A_1")$abundance) # Numeric data from a datafram
 #' @export
 richness <- function(x) {
     x1 <- x[x!=0 & !is.na(x)]
+    stopifnot(x1==as.numeric(x1))
     length(x1)
 }
 
